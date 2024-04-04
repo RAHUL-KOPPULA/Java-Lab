3f89437469c76f548b9cf0270cb1c8decb9fb44d
RAHUL BABU_KOPPULA <160829817+RAHUL-KOPPULA@users.noreply.github.com>
2024-04-04 13:02:09 +0530

    Add files via upload

---
+++ class iteratearr2.java
@@ -0,0 +1,19 @@
+class iteratearr2
+{
+ public static void main(String[]args)
+ {
+  int[] arr={1,2,4,5,6,7};
+  comparearr2 co=new comparearr2();
+  for(int l:co.arr3)
+  { 
+   system.out.println(l);
+  }
+  comparearr2.main(null);
+   System.out.println("elements are");
+
+  for(int i=0;i<arr.length;i++)
+  {
+    System.out.println(arr[i]);
+  }
+ }
+}
